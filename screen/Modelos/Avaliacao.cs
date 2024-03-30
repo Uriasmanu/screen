25847a835b6691427b8a1dd5d86619c85a24b838
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-03-30 11:01:49 -0300

    .

---
+++ screen/Modelos/Avaliacao.cs
@@ -0,0 +1,12 @@
+﻿namespace screen.Modelos;
+using screen.Modelos;
+
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+    public int Nota { get;}
+}
